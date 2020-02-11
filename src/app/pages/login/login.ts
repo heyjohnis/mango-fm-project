b574b1cc0c8664d8a8ea82d732c58d643494de12
Heujoon Kim <joon0227@gmail.com>
2020-02-12 02:31:57 +0900

    bug

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -12,7 +12,7 @@ import { AlertController } from '@ionic/angular'
 })
 export class LoginPage {
 
-	login: UserOptions = { uid: '', user_id: '', email: '', username: '', password: '', file_nm: '' };
+	login: UserOptions = { uid: '', user_id: '', email: '', username: '', password: '', user_type: '', file_nm: '' };
 	submitted = false;
 
 	constructor(
